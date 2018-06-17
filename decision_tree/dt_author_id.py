75cd38399c1d75d533f96b55045d4afcace61aae
Ishwer Kumar <ishwerkumar666@gmail.com>
2018-06-17 13:51:00 +0530

    Add Completed Decision Tree mini Project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# Find number of features in data
+print "\n\nNumber of Features: ", len(features_train[0])
+print "Number of Data Points: ", len(features_train)
+
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "\n\nTraining Time: ", round(time()-t0, 3), "s"
+
+t0 = time()
+score = clf.score(features_test,labels_test)
+print "Testing Time: ", round(time() - t0, 3), "s"
+
+print "\n Accuracy: ", round(score*100,2), "%"
 
 #########################################################
 
