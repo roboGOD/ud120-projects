2a2f2826493c28589fec324773da4bcd0b9ecbf4
Ishwer Kumar <ishwerkumar666@gmail.com>
2018-06-21 14:19:54 +0530

    Completed Regression Mini Project

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,15 +21,17 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,10 +40,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train,target_train)
 
-
-
+print "Slope: ", reg.coef_
+print "Intercept: ", reg.intercept_
+print "r-squared for training: ", reg.score(feature_train, target_train)
+print "r-squared for testing: ", reg.score(feature_test, target_test)
 
 
 
@@ -61,9 +68,17 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    plt.plot( feature_test, reg.predict(feature_test) , linewidth = 2, color = "green")
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="black", linewidth=2) 
+print "New Slope: ", reg.coef_
+print "New Intercept: ", reg.intercept_
+print "New r-squared for training: ", reg.score(feature_test, target_test)
+print "New r-squared for testing: ", reg.score(feature_train, target_train)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
